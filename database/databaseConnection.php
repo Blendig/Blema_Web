8563a18ee5ad577b2b674dc7913279a7702f6520
Blendig <104309667+Blendig@users.noreply.github.com>
2022-04-24 21:48:58 +0200

    database

--- database/databaseConnection.php
+++ database/databaseConnection.php
@@ -6,10 +6,10 @@
 			try{
 				$connect = new PDO('mysql:host=localhost;dbname=BLEMAONLINE', 'root',''); //server, db, username, password
 				$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            	return $connect; //e kthen qat PDO qe me mujt me perdor nfaqe tjera per me bo lidhjen me db
-			}catch(PDOException $e){
+            			return $connect; //e kthen qat PDO qe me mujt me perdor nfaqe tjera per me bo lidhjen me db
+				}catch(PDOException $e){
 				echo "Database Conenction Failed".$e->getMessage(); 
-           		return null;
+           			return null;
 			}
 		}
 	}
