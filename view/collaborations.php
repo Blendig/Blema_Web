c63db562eb87f3ccf4d2563923db78c62416362d
emakutllovci <93519879+emakutllovci@users.noreply.github.com>
2022-04-24 21:11:47 +0200

    js slideshow

--- view/collaborations.php
+++ view/collaborations.php
@@ -8,11 +8,11 @@
             @import url('https://fonts.googleapis.com/css2?family=Orbitron&display=swap');
         </style>
     </head>
-    <body class="body6">
+    <body class="body6">  
         <div class="box6">
             <img id="image">
         </div>
 
-    <script src="../JS/Slideshow.js"></script>
+    <script src="../JS/Slideshow.js"></script>  
     </body>
 </html> 
