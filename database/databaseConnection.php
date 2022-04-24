7d32762787f70fae351f39c7486618111eff855f
Blendig <104309667+Blendig@users.noreply.github.com>
2022-04-24 21:23:15 +0200

    pdo

--- database/databaseConnection.php
+++ database/databaseConnection.php
@@ -8,7 +8,7 @@
 				$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             	return $connect; //e kthen qat PDO qe me mujt me perdor nfaqe tjera per me bo lidhjen me db
 			}catch(PDOException $e){
-				echo "Database Conenction Failed".$e->getMessage();
+				echo "Database Conenction Failed".$e->getMessage(); 
            		return null;
 			}
 		}
